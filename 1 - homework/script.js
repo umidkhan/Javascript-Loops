efcd7b00f6d85c1dcb8adce8d98334e9ce1ea7f8
umidkhan <gameblogger924@gmail.com>
2022-07-18 14:26:43 +0500

    UMIDXON POLATXONOV

---
+++ 1 - homework/script.js
@@ -0,0 +1,12 @@
+let a = +prompt("Yoshingizni kiriting")
+let i = 0
+
+if (a <= 17) {
+    for ( ; i < 1000000000000000n; i++)
+    {
+        +prompt("Qaytatan yoshingizni kiriting!")
+    };
+}
+else {
+    alert("Xush kelibsiz")
+}
---
+++ 2 - homework/script.js
@@ -0,0 +1,13 @@
+let a = +prompt( "Yoshingizni kiriting" )
+
+if (a <= 17) {
+    alert( "Siz armiyaga bormagansiz.\nArmiyaga boring" )
+}
+else if (a > 17) {
+    alert( "Saytimizga xush kelibsiz" )
+    document.write( "Assalomu alaykum saytimizga xush kelibsiz" )
+}
+else {
+    alert( "Nimadir xato ketdi :(\n Qayta urining!" )
+    document.write( "Xato malumot kiritildi😕❌" )
+}
